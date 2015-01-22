055737626d7e8e9eb7ced1b52a055759997ae643
mykokoko <mykokoko38@gmail.com>
2015-01-22 10:33:02 +0900

    자기 소개 / 학교 수정 및 삭제 완료

---
+++ src/pfinal/mypage_info/delete/m_sogaeDelete.java
@@ -0,0 +1,53 @@
+package pfinal.mypage_info.delete;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Vector;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import kit.Command;
+import kit.DBConnectionMgr;
+import dto.listDto;
+
+public class m_sogaeDelete implements Command {
+	@Override
+	public Object processCommand(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		Connection con;
+		PreparedStatement pstmt;
+		ResultSet rs;
+		DBConnectionMgr pool;
+
+
+		HttpSession session = req.getSession();
+		String id = (String) session.getAttribute("id");
+
+		System.out.println("소개 삭제"+id);
+		
+		String sql = "update members set sogae=null where id='"+id +"'";
+		String nowsogae = null;
+		try {
+
+			pool = DBConnectionMgr.getInstance();
+			con = pool.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.executeUpdate();
+			
+
+		} catch (Exception err) {
+			System.out.println("getBoard() : " + err);
+			err.printStackTrace();
+		}
+		
+		System.out.println(nowsogae);
+
+		
+		return "/pfinal.do?command=mylist";
+	}
+}
